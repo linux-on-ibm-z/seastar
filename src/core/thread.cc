628f36e20e6af65e31432c885a73be339ff486c2
Kun-Lu <kun.lu@ibm.com>
2021-03-04 15:37:18 -0500

    Stop backtraces on s390x

--- src/core/thread.cc
+++ src/core/thread.cc
@@ -286,6 +286,9 @@ thread_context::main() {
     asm(".cfi_undefined lr");
 #elif defined(__aarch64__)
     asm(".cfi_undefined x30");
+#elif defined(__s390x__)
+    // Stop backtraces on s390x by marking the return address register r14 as undifined
+    asm(".cfi_undefined r14");
 #else
     #warning "Backtracing from seastar threads may be broken"
 #endif
